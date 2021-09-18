025750b97cb25a69b670e09a36666d5d072d453c
Emmanuel-Gbemou <87377033+Emmanuel-Gbemou@users.noreply.github.com>
2021-09-18 23:44:29 +0200

    first-commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+window.onload = () =>{
+    var collapse = document.getElementById("collapse");
+    var menu = document.getElementById("menu");
+
+    if(collapse){
+        collapse.addEventListener("click", ()=>{
+            console.log("collapse");
+            if(menu.style.display ==="block") {
+                menu.style.display= "none"
+            }else{
+                menu.style.display= "block";
+            }
+        })
+        
+    }
+}
+ 
